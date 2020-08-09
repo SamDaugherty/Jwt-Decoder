b5f648a41a19920bccd0457441da8b3fe709bf7e
Daugherty <samuel_daugherty15@mymail.eku.edu>
2020-08-09 15:28:31 -0400

    jwt decoder

---
+++ JWT_decoder/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Xml;
+
+namespace JWT_decoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+            Console.WriteLine("Enter a JWT: ");
+            var jwtInput = Console.ReadLine();
+
+            // Assume the input is in a control called txtJwtIn,
+            //and the output will be placed in a control called txtJwtOut
+            var jwtHandler = new JwtSecurityTokenHandler(); 
+
+            //Check if readable token (string is in a JWT format)
+            var readableToken = jwtHandler.CanReadToken(jwtInput);
+            var token = jwtHandler.ReadJwtToken(jwtInput);
+
+            //Extract the headers of the JWT
+            var headers = token.Header;
+            var jwtHeader = "{";
+            foreach (var h in headers)
+            {
+                jwtHeader += "\n" + h.Key + "\":\"" + h.Value + "\",";
+            }
+            jwtHeader += "\n}";
+    
+
+            //Extract the payload of the JWT
+            var claims = token.Claims;
+            var jwtPayload = "{";
+            foreach (Claim c in claims)
+            {
+                jwtPayload += "\n" + c.Type + "\":\"" + c.Value + "\",";
+            }
+            jwtPayload += "\n }";
+            Console.WriteLine("JWT Token Is:...");
+            Console.WriteLine("Header: ");
+            Console.WriteLine(jwtHeader);
+            Console.WriteLine("Payload: ");
+            Console.WriteLine(jwtPayload);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Readme
+To run:
+    1) cd int JWT_decoder in a commandline
+    2) execute dotnet run
+    3) enter a JWT (if you don't have one, use mine in the example-jwt folder)
+    
